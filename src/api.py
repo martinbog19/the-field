f264aa3c3d9fbc53bde1dcbbeb9e735abb4cc796
Martin Bogaert <martinbog19@gmail.com>
2026-07-28 12:25:42 +0200

    Add March Madness ESPN odds

--- src/api.py
+++ src/api.py
@@ -4,7 +4,7 @@ import pandas as pd
 class NotFoundError(Exception):
     pass
 
-def get_kalshi_data(event_ticker):
+def get_kalshi_data(event_ticker: str, team_ids = None):
 
     event_ticker = event_ticker.upper().strip()
     url = f"https://api.elections.kalshi.com/trade-api/v2/markets?event_ticker={event_ticker}"
@@ -40,7 +40,7 @@ def get_kalshi_data(event_ticker):
     return df
 
 
-def get_polymarket_data(event_slug):
+def get_polymarket_data(event_slug: str, team_ids = None):
     
     event_slug = event_slug.lower().strip()
     url = f"https://gamma-api.polymarket.com/events/slug/{event_slug}"
@@ -79,5 +79,53 @@ def get_polymarket_data(event_slug):
     prob_sum = df["prob"].sum()
     df["prob"] = 100 * df["prob"] / prob_sum
     df["prob_delta"] = 100 * pd.Series(prob_deltas) / prob_sum
+
+    return df
+
+
+def _american_odds_to_pct(odds: str):
+    odds = float(odds)
+    if odds > 0:
+        return 100 / (odds + 100)
+    else:
+        return -odds / (-odds + 100)
+
+
+def get_espn_data(endpoint: str, team_ids: pd.DataFrame):
+
+    endpoint = endpoint.lower().strip()
+    url = "https://sports.core.api.espn.com/v2/" + endpoint
+    response = requests.get(url)
+
+    data = [x for x in response.json()["items"] if x.get("name", "") == "NCAA(B) - Winner"][0]
+    markets = data["futures"][0]["books"]
+
+    ids, probs = [], []
+    for market in markets:
+
+        odds = market["value"]
+        prob = _american_odds_to_pct(odds)
+        if prob < 0.01:
+            continue
+        probs.append(prob)
+
+        url_tm = market["team"]["$ref"]
+        rhs = url_tm.split("/")[-1]
+        team_id = rhs.split("?")[0] if "?" in rhs else rhs
+        ids.append(team_id)
+
+    df = pd.DataFrame(
+        {
+            "team_id": ids,
+            "prob": probs,
+        }
+    )
+    df["team_id"] = df["team_id"].astype(int)
+    df = df.merge(team_ids, on="team_id", how="left")
+    df["prob_delta"] = 0  # Not implemented yet
+    df["resolved"] = False  # Not implemented yet
+
+    prob_sum = df["prob"].sum()
+    df["prob"] = 100 * df["prob"] / prob_sum
 
     return df
