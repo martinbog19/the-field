f82bbe2f03318c4acc9862faa01830b9c91a4b34
Martin Bogaert <martinbog19@gmail.com>
2026-08-21 00:46:26 -0400

    oops

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ with dd_col:
     room_id = "martin" if room == "Martin's friends" else "krish"
 
 is_mobile = screen_width is not None and screen_width < 640
-is_mobile = True
 
 
 with open(f"data/{room_id}/leagues.txt", "r") as f:
